bfaac2a4a61dc90e98c6b26fffc9175722f34fd7
Alex Moiseev <coding-awm@mail.ru>
2020-06-27 21:14:18 +0600

    сделал первую часть урока 14

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 'use strict'
 
 let body = document.querySelector('body');
+
 let DomElement = function () {
-    this.selector;
-    this.height;
-    this.width;
-    this.bg;
-    this.fontSize;
+    this.selector = 0;
+    this.height = '100px';
+    this.width = '100px';
+    this.bg = 'red';
+    this.fontSize = '25px';
 }
 
-let create = function (z) {
+DomElement.prototype.create = function (z) {
     let temp = z[0];  
     let temp2 = z.slice(1) 
-    let newElem; 
+    let newElem = this.selector; 
     
     if (temp === '.') {
         newElem = document.createElement('div');
@@ -22,12 +23,14 @@ let create = function (z) {
         newElem.setAttribute('id', temp2)
     }
     
-    body.append(newElem);
-    newElem.textContent = prompt('Введите текст','Hello');
-    newElem.style.width = '100px';
-    newElem.style.height = '100px';
-    newElem.style.backgroundColor = 'green';
-    newElem.style.fontSize = '25px';
-    console.dir(newElem);
+    body.prepend(newElem);
+    newElem.textContent = 'Ghbdtdn';
+    newElem.style.width = this.width;
+    newElem.style.height = this.height;
+    newElem.style.backgroundColor = this.bg;
+    newElem.style.fontSize = this.fontSize;
 }
-create('.class');
+
+let element = new DomElement
+element.create(prompt('если вам нужно создать класс, то начните писать название класса с точки, если же нужен параграф с id?  то начните писать название id с решетки','Hello'));
+
