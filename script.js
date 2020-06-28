62b6ef1d744c2d03509763e0221ce565fdc198b7
Alex Moiseev <coding-awm@mail.ru>
2020-06-28 16:45:10 +0600

    работает с промптами

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ DomElement.prototype.create = function () {
     this.selector = prompt('если вам нужно создать класс, то начните писать название класса с точки, если же нужен параграф с id?  то начните писать название id с решетки', '.Hello')
     let temp = this.selector[0];
     let temp2 = this.selector.slice(1)
-    let newElem = this.selector;
+    let newElem;
 
     if (temp === '.') {
         newElem = document.createElement('div');
@@ -23,10 +23,12 @@ DomElement.prototype.create = function () {
         newElem = document.createElement('p');
         newElem.setAttribute('id', temp2)
     }
+
     this.height = prompt('Введите высоту', '100');
     this.width = prompt('Введите ширину', '100');
     this.bg = prompt('Введите цвет фона', 'green');
     this.fontSize = prompt('Введите размер шрифта', '25');
+
     body.prepend(newElem);
     newElem.style.cssText = `color: red;
     background-color: ${this.bg};
@@ -40,5 +42,5 @@ DomElement.prototype.create = function () {
 let element = new DomElement;
 element.create();
 
-let element2 = new DomElement;
-element2.create();
+// let element2 = new DomElement;
+// element2.create();
